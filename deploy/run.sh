7d52d9b69b8c172fdd25970f1cca54019e0bce3d
Lex Meng <9191288+lexmeng@users.noreply.github.com>
2022-02-13 01:27:54 +0800

    test4

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,17 +3,16 @@ succ=1
 function check_dead_link(){
     for file in `ls $1`
     do
-        echo file
         if [ -d $1"/"$file ]; then
-            echo $1"/"$file $2
+            check_dead_link $1"/"$file $2
         elif [ "${file: 0-5 :5}" == ".yaml" ]; then
             echo $file
-            succ=0
         fi
     done
 }
 echo "Start to check dead links."
 check_dead_link deploy
+check_dead_link src
 if [ $succ -eq 0 ]; then
     echo "Found dead links, please find logs above."
     exit 1
